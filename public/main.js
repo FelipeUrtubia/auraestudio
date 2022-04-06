7cb319e97da265304625237ca78cea318f793291
Felipe Urtubia <pipe-3-97@hotmail.com>
2022-04-05 22:00:45 -0400

    primer commit, base del proyecto

---
+++ public/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3L7a8xtJitS1zJKqvxUJc-JYU-w9eTZY",
+  authDomain: "auraestudio.firebaseapp.com",
+  projectId: "auraestudio",
+  storageBucket: "auraestudio.appspot.com",
+  messagingSenderId: "549065503509",
+  appId: "1:549065503509:web:80bcd642aec5fa4592ef1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
